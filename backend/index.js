bb23d543d09634cd3ef5c467db5a783ae1faf719
Kartikey Sharma <skartikey1199@gmail.com>
2024-11-19 20:34:41 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(
     origin: function (origin, callback) {
       console.log("Request from origin:", origin); // Log origin for debugging
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(Access-Control-Allow-origin, true);
+        callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
